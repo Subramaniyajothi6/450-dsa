4c24600cde216cb4008d65f9182091a3384913bd
Subramaniyajothi6 <subramaniyajothi2002@gmail.com>
2026-05-24 00:42:24 +0530

    fix run.py

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
-from app import create_app
+from flask import Flask
 
+app = Flask(__name__)
 
-app = create_app()
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/")
+def home():
+    return "Vercel Flask Working"
